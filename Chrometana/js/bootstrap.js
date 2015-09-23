eaf7324a23fe345b9528033369214fbfe156f984
Kyle Ladd <kladd6@gmail.com>
2015-09-22 22:32:38 -0400

    Custom Search Engine

--- Chrometana/js/bootstrap.js
+++ Chrometana/js/bootstrap.js
@@ -1,52 +1,36 @@
-var storageChange="Google.com";
-function convertURL(url){
-    console.log(url)
-    url = url.replace(/%20/g,"+")
-    var uri = /\?q\=([0-9a-zA-Z-._~:\/?#[\]@!$'()*+,;=%]*)($|(\&))/.exec(url)[1]
-    console.log(url)
-    console.log(uri)
+var defaultEngine = "https://www.google.com/search?q=";
+var storageChange = defaultEngine;
+var debugging = false;
 
+function convertURL(url){
+    log(url)
+    url = url.replace(/%20/g,"+");
+    var uri = /\?q\=([0-9a-zA-Z-._~:\/?#[\]@!$'()*+,;=%]*)($|(\&))/.exec(url)[1];
+    log(url);
+    log(uri);
+    log(storageChange);
     var match = /^((go\+to\+)|(open\+)|())([0-9a-zA-Z-._~:\/?#[\]@!$'()*+,;=%]*\.[a-z]+)/i.exec(uri)
-    console.log(match)
+    log(match)
     if(match){
         return "http://" + match[5]
     }
-    if(url.search("search?q=go%20to%20")!=-1){
+    if(url.search("search?q=go%20to%20") != -1){
         return "http://" + url.substring(url.search('=')+11, url.search('&'));
     }
-    if(storageChange=="Google.com"){
-        return "https://www.google.com/search?q=" + uri;
-    }
-    if(storageChange=="DuckDuckGo.com"){
-        return "https://www.duckduckgo.com?q=" + uri;
-    }
-    if(storageChange=="Yahoo.com"){
-        return "https://search.yahoo.com/search?p=" + uri;
-    }
-    if(storageChange=="Custom"){
-        return custom_engine + uri
-    }
-    return url.replace("www.bing.com/search", "www.google.com/search");
+    return storageChange + uri;
 }
 chrome.storage.sync.get('search_engine', function (obj) {
-    console.log('myKey', obj);
-    if(storageChange == "Custom"){
-        custom_engine = changes['custom_engine']['newValue']
-    }
-    storageChange=obj['search_engine'];
+    log('myKey', obj);
+    storageChange = obj['search_engine'];
 });
-
 chrome.storage.onChanged.addListener(function(changes, namespace) {  
+    log(changes);
     storageChange = changes['search_engine']['newValue'];
-    if(storageChange == "Custom"){
-        custom_engine = changes['custom_engine']['newValue']
-    }
-    console.log(storageChange);
+    log(storageChange);
 });
 
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
-   	console.log(storageChange);
-    
+   	log(storageChange);
     return { redirectUrl: convertURL(details.url)};
 }, {urls: ["*://www.bing.com/search*"]}, ["blocking"]);
 
@@ -54,7 +38,8 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
 chrome.runtime.onInstalled.addListener(function(details){
     if(details.reason == "install"){
         chrome.tabs.create({url: "html/options.html?newinstall=yes"});
-    }else if(details.reason == "update"){
+    }
+    else if(details.reason == "update"){
         var thisVersion = chrome.runtime.getManifest().version;
         console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
     }
@@ -65,5 +50,13 @@ function onMessage(request, sender, callback) {
     if (request.action == "convertURL") {
         callback(convertURL(request.url));
     }
+    else if(request.action == "defaultEngine"){
+        callback(defaultEngine);
+    }
     return true;
+}
+function log(txt) {
+    if(debugging) {
+      console.log(txt);
+    }
 }
--- Chrometana/js/options.js
+++ Chrometana/js/options.js
@@ -1,7 +1,6 @@
-function save_options(element, value) {
-  var search_engine = value
+function save_options(element,value) {
   chrome.storage.sync.set({
-    search_engine: search_engine
+    search_engine: value
   }, function() {
     for (i = 0; i <  selectorList.length; i++) {
       removeClass(selectorList[i], 'selected');
@@ -11,32 +10,36 @@ function save_options(element, value) {
     status.textContent = 'New search engine preferences saved.';
     setTimeout(function() {
       status.textContent = '';
-    }, 750);
+    }, 1000);
   });
-
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  chrome.storage.sync.get({
-    search_engine: 'Google.com'
-  }, function(items) {
-    for (i = 0; i <  selectorList.length; i++) {
-      if (selectorList[i].getAttribute('value') == items.search_engine) {
-        addClass(selectorList[i], 'selected');
-      } else {
-        removeClass(selectorList[i], 'selected');
-      }
-    }
-  });
+  chrome.runtime.sendMessage({"action": "defaultEngine"},
+    function (defaultEngine) {
+      chrome.storage.sync.get({
+        search_engine: defaultEngine
+      }, function(items) {
+        for (i = 0; i <  selectorList.length; i++) {
+          if (selectorList[i].getAttribute('value') == items.search_engine) {
+            addClass(selectorList[i], 'selected');
+          }
+          else {
+            removeClass(selectorList[i], 'selected');
+          }
+        }
+        document.getElementById("custom_engine").value = items.search_engine;
+      });
+    });
 }
 
 //Parses arguments from URL
 function getURLVariable(variable){
   var query = window.location.search.substring(1);
   var vars = query.split("&");
-  for (var i=0;i<vars.length;i++) {
+  for (var i = 0; i < vars.length; i++) {
     var pair = vars[i].split("=");
     if(pair[0] == variable){return pair[1];}
   }
@@ -52,28 +55,22 @@ if (getURLVariable("newinstall") == "yes"){
   installadvice.textContent = 'To come back to this page at any time, go to Chrome Settings, open Extensions, and click Options underneath Chrometana';
 }
 
-
 for (i = 0; i <  selectorList.length; i++) {
   selectorList[i].addEventListener('click', function() {
-    save_options(this, this.getAttribute('value'))
+    save_options(this,this.getAttribute("value"));
   });
 }
 
 document.getElementById('custom_engine_update').addEventListener('click', function() {
-  handleUpdateEngine(document.getElementById('custom_engine'));
-})
-
-function handleUpdateEngine(element) {
-  engine = element.value
-  //Insert code here to update engine
-}
+  var element = document.getElementById('custom_engine');
+  save_options(element,element.value);
+});
 
 function addClass(element, classNameToAdd) {
   if (!element.className.includes(classNameToAdd)) {
     element.className = element.className + ' ' + classNameToAdd;
   }
 }
-
 
 function removeClass(element, classNameToAdd) {
   element.className = element.className.replace(classNameToAdd, '');
